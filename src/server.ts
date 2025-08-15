99fa42c437a2e301639f5f2e7c5185e309710a57
Onyinyechukwu Adesanya <yinadesanya@gmail.com>
2025-08-15 23:32:20 +0100

    feat: add body-parser and jsonwebtoken dependencies; configure body-parser in server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
